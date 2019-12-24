96fc278b228306abcf9453946ecf366988544eec
Anton Voloshyn <avoloshyn111@gmail.com>
2019-12-24 22:19:44 +0200

    Lab9

---
+++ dk72_voloshyn/lab9_cache/src/instr_mem.sv
@@ -0,0 +1,20 @@
+//Code was taken from Oleh "Rabu" Matiusha
+
+`timescale 1ns/1ps
+
+
+module instr_mem(
+	input logic [31:0] addr,
+	output logic [31:0] rd);
+
+	parameter capacity = 128;
+	localparam addr_width = $clog2(capacity);
+
+	logic [31:0] ROM [capacity-1:0];
+
+	wire [addr_width-1:0] addr_stripped = addr[0+:addr_width];
+
+
+	initial $readmemh("/home/sad/Lab9Cache/memfile.dat", ROM);
+	assign rd = ROM[addr_stripped]; // word aligned
+endmodule
